2bf589f930e598183e221a24def37b8eb90cad97
mmadajski <80204566+mmadajski@users.noreply.github.com>
2023-10-02 11:04:02 +0200

    Added saving features names selected by models

--- Telco_churn_prediction.py
+++ Telco_churn_prediction.py
@@ -10,16 +10,12 @@ from sklearn.linear_model import LogisticRegression
 from Utils import calculate_metrics
 
 
-
 # Reading data and dropping rows with missing values
 data = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
-print(data.columns)
-print("d1: ", data.shape)
 data.replace(" ", np.nan, inplace=True)
 data.dropna(inplace=True)
 data.reset_index(inplace=True, drop=True)
 data["TotalCharges"] = data["TotalCharges"].astype("float64")
-print("d1: ", data.shape)
 data.drop("customerID", axis="columns", inplace=True)
 
 """
@@ -36,7 +32,6 @@ selected_rows = sample([i for i in range(len(zero_class))], 2 * len(one_class))
 selected_zero_class = zero_class.iloc[selected_rows]
 
 data = pd.concat([selected_zero_class, one_class, one_class])
-#data = pd.concat([zero_class, one_class, one_class])
 data.drop("index", axis="columns", inplace=True)
 data.reset_index(inplace=True)
 
@@ -67,7 +62,6 @@ scaler.fit(data_train[["tenure", "MonthlyCharges", "TotalCharges"]])
 data_train[["tenure", "MonthlyCharges", "TotalCharges"]] = scaler.transform(data_train[["tenure", "MonthlyCharges", "TotalCharges"]])
 data_test[["tenure", "MonthlyCharges", "TotalCharges"]] = scaler.transform(data_test[["tenure", "MonthlyCharges", "TotalCharges"]])
 
-
 # Logistic Regression
 logistic_regression = LogisticRegression(penalty="elasticnet", C=0.01, l1_ratio=0.8, solver="saga")
 logistic_regression.fit(data_train, answers_train)
@@ -82,27 +76,25 @@ roc_lr = pd.DataFrame({"fpr": fpr, "tpr": tpr})
 roc_lr.to_csv("lr_roc_data.csv", sep=";", decimal=",")
 
 # Variables used in the logistic regression model
-rejected_variables = logistic_regression.coef_
-selected_vars = data.columns[[x != 0 for x in rejected_variables[0]]]
-
-print("selected variables: ", list(selected_vars))
-print("rejected: ", list(set(data.columns) - set(selected_vars)))
-
+regression_coefficients = logistic_regression.coef_
+selected_vars = data.columns[[x != 0 for x in regression_coefficients[0]]]
+regression_features = pd.DataFrame(selected_vars)
+regression_features.to_csv("logistic_regression_features.csv")
 
 # Decision tree.
 decision_tree = tree.DecisionTreeClassifier(max_depth=5, min_samples_split=30, random_state=123)
 ada_boost_tree = AdaBoostClassifier(n_estimators=30, estimator=decision_tree, learning_rate=1)
 ada_boost_tree.fit(data_train, answers_train)
 
-
 train_prob_tree = ada_boost_tree.predict_proba(data_train)[:, 1]
 test_prob_tree = ada_boost_tree.predict_proba(data_test)[:, 1]
 metrics_train_tree = pd.DataFrame(calculate_metrics(answers_train, train_prob_tree, "tree_train"))
 metrics_test_tree = pd.DataFrame(calculate_metrics(answers_test, test_prob_tree, "tree_test"))
 
-dsdf = data.columns[[i > 0 for i in ada_boost_tree.feature_importances_]]
-print(dsdf)
-print(len(dsdf))
+tree_feature_importance = data.columns[[i != 0 for i in ada_boost_tree.feature_importances_]]
+tree_feature = pd.DataFrame(tree_feature_importance)
+tree_feature.to_csv("tree_features.csv")
+
 metrics_data = pd.concat([metrics_train_lr, metrics_test_lr, metrics_train_tree, metrics_test_tree])
 metrics_data.to_csv("models_metrics.csv")
 
