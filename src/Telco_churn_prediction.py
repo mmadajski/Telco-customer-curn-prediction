0de9007c1cc9f4306ed3ecae328effdfc8d7f526
mmadajski <80204566+mmadajski@users.noreply.github.com>
2023-11-12 15:00:51 +0100

    Teclo_churn_prediction refactor

--- src/Telco_churn_prediction.py
+++ src/Telco_churn_prediction.py
@@ -52,27 +52,27 @@ for column in variables_one_hot_encoding:
     data.drop(column, axis="columns", inplace=True)
 
 # Train-test split
-answers = data.pop("Churn")
+Y = data.pop("Churn")
 data.drop("index", axis="columns", inplace=True)
-data_train, data_test, answers_train, answers_test = train_test_split(data, answers, test_size=0.3, random_state=123)
+X_train, X_test, Y_train, Y_test = train_test_split(data, Y, test_size=0.3, random_state=123)
 
 # Data scaling
 scaler = StandardScaler()
-scaler.fit(data_train[["tenure", "MonthlyCharges", "TotalCharges"]])
+scaler.fit(X_train[["tenure", "MonthlyCharges", "TotalCharges"]])
 
-data_train[["tenure", "MonthlyCharges", "TotalCharges"]] = scaler.transform(data_train[["tenure", "MonthlyCharges", "TotalCharges"]])
-data_test[["tenure", "MonthlyCharges", "TotalCharges"]] = scaler.transform(data_test[["tenure", "MonthlyCharges", "TotalCharges"]])
+X_train[["tenure", "MonthlyCharges", "TotalCharges"]] = scaler.transform(X_train[["tenure", "MonthlyCharges", "TotalCharges"]])
+X_test[["tenure", "MonthlyCharges", "TotalCharges"]] = scaler.transform(X_test[["tenure", "MonthlyCharges", "TotalCharges"]])
 
 # Logistic Regression
 logistic_regression = LogisticRegression(penalty="elasticnet", C=0.01, l1_ratio=0.8, solver="saga")
-logistic_regression.fit(data_train, answers_train)
+logistic_regression.fit(X_train, Y_train)
 
-train_prob_lr = logistic_regression.predict_proba(data_train)[:, 1]
-test_prob_lr = logistic_regression.predict_proba(data_test)[:, 1]
-metrics_train_lr = pd.DataFrame(calculate_metrics(answers_train, train_prob_lr, "lr_train"))
-metrics_test_lr = pd.DataFrame(calculate_metrics(answers_test, test_prob_lr, "lr_test"))
+train_prob_lr = logistic_regression.predict_proba(X_train)[:, 1]
+test_prob_lr = logistic_regression.predict_proba(X_test)[:, 1]
+metrics_train_lr = pd.DataFrame(calculate_metrics(Y_train, train_prob_lr, "lr_train"))
+metrics_test_lr = pd.DataFrame(calculate_metrics(Y_test, test_prob_lr, "lr_test"))
 
-fpr, tpr, _ = roc_curve(answers_test, test_prob_lr)
+fpr, tpr, _ = roc_curve(Y_test, test_prob_lr)
 roc_lr = pd.DataFrame({"fpr": fpr, "tpr": tpr})
 roc_lr.to_csv("lr_roc_data.csv", sep=";", decimal=",")
 
@@ -85,12 +85,12 @@ regression_features.to_csv("logistic_regression_features.csv")
 # Decision tree
 decision_tree = tree.DecisionTreeClassifier(max_depth=5, min_samples_split=30, random_state=123)
 ada_boost_tree = AdaBoostClassifier(n_estimators=30, estimator=decision_tree, learning_rate=1)
-ada_boost_tree.fit(data_train, answers_train)
+ada_boost_tree.fit(X_train, Y_train)
 
-train_prob_tree = ada_boost_tree.predict_proba(data_train)[:, 1]
-test_prob_tree = ada_boost_tree.predict_proba(data_test)[:, 1]
-metrics_train_tree = pd.DataFrame(calculate_metrics(answers_train, train_prob_tree, "tree_train"))
-metrics_test_tree = pd.DataFrame(calculate_metrics(answers_test, test_prob_tree, "tree_test"))
+train_prob_tree = ada_boost_tree.predict_proba(X_train)[:, 1]
+test_prob_tree = ada_boost_tree.predict_proba(X_test)[:, 1]
+metrics_train_tree = pd.DataFrame(calculate_metrics(Y_train, train_prob_tree, "tree_train"))
+metrics_test_tree = pd.DataFrame(calculate_metrics(Y_test, test_prob_tree, "tree_test"))
 
 # Decision tree feature importance
 tree_feature_importance = data.columns[[i != 0 for i in ada_boost_tree.feature_importances_]]
@@ -101,6 +101,6 @@ tree_feature.to_csv("tree_features.csv")
 metrics_data = pd.concat([metrics_train_lr, metrics_test_lr, metrics_train_tree, metrics_test_tree])
 metrics_data.to_csv("models_metrics.csv", sep=";", decimal=",")
 
-fpr, tpr, _ = roc_curve(answers_test, test_prob_tree)
+fpr, tpr, _ = roc_curve(Y_test, test_prob_tree)
 roc_tree = pd.DataFrame({"fpr": fpr, "tpr": tpr})
 roc_tree.to_csv("tree_roc_data.csv", sep=";", decimal=",")
